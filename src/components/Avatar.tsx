c7de4bcdb1e74c1a65e4d0ca1f05c1ac9d3c83a6
Juan Jose Carracedo <djwayomix@gmail.com>
2020-11-30 20:00:00 +0100

    chore: added avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Image,
+  StyleProp,
+  ViewStyle,
+  GestureResponderEvent,
+  Text,
+  View,
+  TextStyle,
+  ImageSourcePropType,
+} from 'react-native';
+import { withTheme } from '../core/Theme';
+
+interface AvatarProps {
+  theme: ReactNativeNoxi.Theme;
+  src?: ImageSourcePropType;
+  text?: any;
+  color?: string;
+  textColor?: string;
+  size?: number;
+  onPress?: (event: GestureResponderEvent) => void;
+  type?: 'round' | 'square';
+  style?: StyleProp<ViewStyle>;
+  styleText?: StyleProp<TextStyle>;
+}
+
+const Avatar = ({
+  src,
+  text,
+  size = 60,
+  type = 'round',
+  styleText,
+  onPress,
+  color,
+  theme,
+  textColor,
+  style,
+}: AvatarProps) => {
+  const avatarStlye: StyleProp<any> = {
+    width: size,
+    height: size,
+    backgroundColor: theme.colors.primary,
+  };
+
+  const textStyle: StyleProp<any> = {
+    fontSize: size / 2,
+    color: theme.colors.buttonText,
+  };
+
+  if (type === 'round') {
+    avatarStlye.borderRadius = size / 2;
+  }
+
+  if (type === 'square') {
+    avatarStlye.borderRadius = size / 8;
+  }
+
+  if (color) {
+    avatarStlye.backgroundColor = color;
+  }
+
+  if (textColor) {
+    textStyle.color = textColor;
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={[styles.container, avatarStlye, style]}>
+        {src ? (
+          <Image source={src} style={avatarStlye} />
+        ) : (
+          <Text adjustsFontSizeToFit={true} style={[textStyle, styleText]}>
+            {text}
+          </Text>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default withTheme(Avatar);
