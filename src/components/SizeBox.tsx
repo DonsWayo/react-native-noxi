9e9e464bf57e9787f81a007fcc2967310f83d15a
Juan Jose Carracedo <djwayomix@gmail.com>
2020-12-05 01:25:42 +0100

    fix: sizebox file names

--- src/components/SizeBox.tsx
+++ src/components/SizeBox.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-interface componentNameProps {
+interface SizeBoxProps {
   height?: number | string;
   width?: number | string;
 }
 
-const componentName = ({ height = 10, width = 10 }: componentNameProps) => {
+const SizeBox = ({ height = 10, width = 10 }: SizeBoxProps) => {
   return <View style={[styles.container, { height, width }]} />;
 };
 
-export default componentName;
+export default SizeBox;
 
 const styles = StyleSheet.create({
   container: { backgroundColor: 'transparent' },
